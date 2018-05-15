240ac7514ada419556dadb37a58ddaa682f87395
PanupanSekitori <ball085573@hotmail.com>
2018-05-15 14:06:25 +0700

    .

--- index.js
+++ index.js
@@ -27,32 +27,38 @@ app.get('/get',function(req,res){
   var data_transactions;
   var data_sort;
   var data_sort_ten = [];
+  var Json_10;
 
   unirest.get('https://testnet.blockexplorer.com/api/addr/mzJDNGyyE6HFVoZAFB82ZVX74kmFcTPey4',function(req,res){
     var dataX= req.raw_body;
-    //console.log(req.body);
-
-    //view transactions OK
-    //console.log(req.body.transactions);
     data_transactions = req.body.transactions;
-    //dataX.push(req.raw_body);
-
     data_sort = data_transactions;
-    // console.log("All = ",data_sort.length); //จำนวนทั้งหมด
-    // console.log("All = ",data_sort.length - 10); //จำนวนทั้งหมด -10
-
     console.log("###############################################################");
-      data_sort_ten.push(data_sort[165]); //เอาข้อมูลใน array ของ arr1 เข้าไปใน totalX
-      data_sort_ten.push(data_sort[166]); //เอาข้อมูลใน array ของ arr1 เข้าไปใน totalX
-      data_sort_ten.push(data_sort[170]); //เอาข้อมูลใน array ของ arr1 เข้าไปใน totalX
-      data_sort_ten.push(data_sort[169]); //เอาข้อมูลใน array ของ arr1 เข้าไปใน totalX
-      data_sort_ten.push(data_sort[168]); //เอาข้อมูลใน array ของ arr1 เข้าไปใน totalX
-      data_sort_ten.push(data_sort[167]); //เอาข้อมูลใน array ของ arr1 เข้าไปใน totalX
-      data_sort_ten.push(data_sort[173]); //เอาข้อมูลใน array ของ arr1 เข้าไปใน totalX
-      data_sort_ten.push(data_sort[172]); //เอาข้อมูลใน array ของ arr1 เข้าไปใน totalX
-      data_sort_ten.push(data_sort[171]); //เอาข้อมูลใน array ของ arr1 เข้าไปใน totalX
-      data_sort_ten.push(data_sort[164]); //เอาข้อมูลใน array ของ arr1 เข้าไปใน totalX
+      var A1 = data_sort[165];
+      var A2 = data_sort[166];
+      var A3 = data_sort[170];
+      var A4 = data_sort[169];
+      var A5 = data_sort[168];
+      var A6 = data_sort[167];
+      var A7 = data_sort[173];
+      var A8 = data_sort[172];
+      var A9 = data_sort[171];
+      var A10 = data_sort[164];
 
+  data_sort_ten.push(A1); //เอาข้อมูลใน array ของ arr1 เข้าไปใน
+  data_sort_ten.push(A2); //เอาข้อมูลใน array ของ arr1 เข้าไปใน
+  data_sort_ten.push(A3); //เอาข้อมูลใน array ของ arr1 เข้าไปใน
+  data_sort_ten.push(A4); //เอาข้อมูลใน array ของ arr1 เข้าไปใน
+  data_sort_ten.push(A5); //เอาข้อมูลใน array ของ arr1 เข้าไปใน
+  data_sort_ten.push(A6); //เอาข้อมูลใน array ของ arr1 เข้าไปใน
+  data_sort_ten.push(A7); //เอาข้อมูลใน array ของ arr1 เข้าไปใน
+  data_sort_ten.push(A8); //เอาข้อมูลใน array ของ arr1 เข้าไปใน
+  data_sort_ten.push(A9); //เอาข้อมูลใน array ของ arr1 เข้าไปใน
+  data_sort_ten.push(A10); //เอาข้อมูลใน array ของ arr1 เข้าไปใน
+
+  console.log("data_sort_ten == ",data_sort_ten.length);
+  //console.log("data_sort_ten == ",data_sort_ten[2]);
+      console.log("\n");
       console.log("All 10  = \n",data_sort_ten);
   });
 
@@ -65,32 +71,90 @@ app.get('/get',function(req,res){
         time : req.body.time
       };
 
-      console.log("\n");
       sort_x.push(dataJ);
 
       console.log(sort_x[0]);
-      console.log("get OK");
       //dataX.push(req.raw_body);
     });
-  return;
+
+    unirest.get('https://testnet.blockexplorer.com/api/tx/fe2ac15767905041726d226ff0b2fe67b90490daf57bc1106974d217b044083b',function(req,res){
+      //var dataX= req.raw_body;
+      var dataJ ={
+        txid : req.body.txid,
+        time : req.body.time
+      };
+
+      sort_x.push(dataJ);
+
+      console.log(sort_x[1]);
+      //dataX.push(req.raw_body);
+    });
+
 });
 
 app.get('/get2',function(req,res){
-  var data2;
-  unirest.get('https://testnet.blockexplorer.com/api/tx/34862264c23b471a8b2e7199e2b3d477bff72cf526d0bffb6b2de83ff6f596a2 ',function(req,res){
-    //var dataX= req.raw_body;
-    // data2 = [
-    //   {"txid" : req.body.txid,
-    //   "time" : req.body.time},
-    // ]
-    console.log(req.raw_body);
-    //dataX.push(req.raw_body);
-  });
+  data_10 = [];
+  arr2 = [];
+    var A1 = "https://testnet.blockexplorer.com/api/tx/34862264c23b471a8b2e7199e2b3d477bff72cf526d0bffb6b2de83ff6f596a2";
+    var A2 = "https://testnet.blockexplorer.com/api/tx/b3d6b0318b2e1c96710b329c28f1bb3e64c6069ded8c378398ff7fe6bf8db75e";
+    var A3 = "https://testnet.blockexplorer.com/api/tx/67455345c850ece8da18a3f2e14360f764aed727572b6f89c905072f813f576b";
+    var A4 = "https://testnet.blockexplorer.com/api/tx/b3928f2be03fc461c0b09611b1a9a48183aaffe574a87ca6613d454ee0e3af20";
+    var A5 = "https://testnet.blockexplorer.com/api/tx/d372f8a2369d9c8a42af4ca7a1f8c82b4a3c2a72f7b5a83d42dc9f48c0327fc5";
+    var A6 = "https://testnet.blockexplorer.com/api/tx/55ff8b0a547281c3b3da1d3fc6b8427ae49651025d82b8a0ac7be2f4062d1a13";
+    var A7 = "https://testnet.blockexplorer.com/api/tx/8f5fef9a35050b036e36d41f7fa43e95227c0a5941ab60946dec2f5478e3a74f";
+    var A8 = "https://testnet.blockexplorer.com/api/tx/10e17bd990a4313b473882779e70ca9e72da39db5086d6f703e7778aa1ce4749";
+    var A9 = "https://testnet.blockexplorer.com/api/tx/4fa1e1257dd5f180e175082d40ac1cbd7e4e24dc481ef9f6d5befa88320bd70d";
+    var A10 = "https://testnet.blockexplorer.com/api/tx/6afe7a50ef8c4f582877ba6b7cf068f1c8f03cec5bea97a4c3ed725b182d55ec";
+      arr2.push(A1);
+      arr2.push(A2);
+      arr2.push(A3);
+      arr2.push(A4);
+      arr2.push(A5);
+      arr2.push(A6);
+      arr2.push(A7);
+      arr2.push(A8);
+      arr2.push(A9);
+      arr2.push(A10);
 
-  setTimeout(function(){
-    console.log("get OK");
-  },1000);
-  return;
+
+
+    for(var i = 0; i < arr2.length; i++){ //นับว่าใน arr1 มีข้อมูลใน array กี่ตัว แล้วทำ loop ตามจำนวนนั้น
+      console.log(arr2[i]); //แสดง totalX
+      unirest.get(arr2[i],function(req,res){
+        //var dataX= req.raw_body;
+        var dataJ ={
+          txid : req.body.txid,
+          time : req.body.time
+        };
+        data_10.push(dataJ);
+        console.log(dataJ);
+        //dataX.push(req.raw_body);
+      });
+    }
+
+
+    setTimeout(function(){
+      var totalX = [];
+
+      console.log("-----------------------------------------------------------");
+      for(var i = 0; i < data_10.length; i++){ //นับว่าใน arr1 มีข้อมูลใน array กี่ตัว แล้วทำ loop ตามจำนวนนั้น
+          totalX.push(data_10[i].time); //เอาข้อมูลใน array ของ arr1 เข้าไปใน totalX
+          console.log("\n",totalX);
+      }
+
+      totalX.sort(function(a, b){ //ในกรณีที่ต้องการเรียงข้อมูลที่เป็นตัวเลขเราจำเป็นต้องเขียน callback function เพิ่มเข้าไปใน function sort เพื่อให้การเรียงข้อมูลอยู่ในรูปแบบที่ถูกต้อง
+        return a-b}
+      );
+
+      console.log("\n"+totalX); // 1,4,7,9,10,28,35,200
+
+  },1500);
+
+
+
+
+
+
 });
 
 
